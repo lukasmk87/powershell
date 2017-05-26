303770d3d108aa9a21fbb3aad98fff080a5a2407
Lukas Matthäus Kotowicz <lukaskotowicz@Gmail.com>
2017-05-26 15:10:21 +0200

    Add files via upload

---
+++ CSV_SORT.ps1
@@ -0,0 +1,8 @@
+﻿$datei1= Import-CSV 'C:\Users\lukaskotowicz.BUR-KG\Desktop\zahlweg_verteilung_von_160401_bis_170207.csv' | sort bnr -Descending
+$datei2= Import-CSV 'C:\Users\lukaskotowicz.BUR-KG\Desktop\zahlweg_verteilung_von_160401_bis_170207 - Kopie.csv' | sort bnr -Descending
+$datei1_sort= Export-Csv -Path $datei1+sort -NoTypeInformation
+$datei2_sort= Export-Csv -Path $datei2+sort -NoTypeInformation
+
+$datei1 | Export-Csv -Path 'C:\Users\lukaskotowicz.BUR-KG\Desktop\zahlweg_verteilung_von_160401_bis_170207_sort.csv' -NoTypeInformation
+$datei2 | Export-Csv -Path 'C:\Users\lukaskotowicz.BUR-KG\Desktop\zahlweg_verteilung_von_160401_bis_170207 - Kopie_sort.csv' -NoTypeInformation
+
---
+++ Datev_Backup.ps1
@@ -0,0 +1,23 @@
+﻿$onlinebackup= "D:\DATEV\PROGRAMM\DBMSTOOL\DVPCDBMSCHECKCONSOLE711.EXE D:\DATEV\DATEN\DBMSTOOL\DATA01\STANDARD\STDMSCHECK_LOCALHOST.XML"
+
+cmd /c $onlinebackup
+
+Stop-Service -DisplayName "SQL Server (DATEV_DBENGINE)"
+
+copy -r D:\DATEV\ D:\DATEV_SICHERUNG\DATEISICHERUNG\DATEV
+copy -r D:\WINDSVSW1 D:\DATEV_SICHERUNG\DATEISICHERUNG\WINDSVSW1
+
+Start-Service -DisplayName "SQL Server (DATEV_DBENGINE)"
+
+
+set-alias sz "$env:ProgramFiles\7-Zip\7z.exe"
+$date = Get-Date -Format yyyy-MM-dd
+
+$Source = "D:\DATEV_SICHERUNG\"
+$Target = "\\srvfs01\datastore-vol5\Datev_Sicherung\Sicherung_$date.zip"
+
+sz a -mx=9 $Target $Source
+
+Remove-Item $Source -Recurse -Force
+
+Send-MailMessage -To "Lukas Kotowwicz <lukas.kotowicz@bur-kg.de>" -From "Datev Sicherung <datev.backup@bur-kg.de>" -Subject "Backup erfolgreich" -SmtpServer "smtp-relay.bur-kg.org"
